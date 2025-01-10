59ffdd53d96a61247f1750bbbedd054363143dd4
Kaito Ono <kaito@example.com>
2025-01-11 07:19:32 +0900

    アルコールの強さを表示させること

---
+++ db/migrate/20250110200651_change_alcohol_strength_to_integer_in_recipes.rb
@@ -0,0 +1,11 @@
+class ChangeAlcoholStrengthToIntegerInRecipes < ActiveRecord::Migration[7.0]
+  def change
+    execute <<-SQL
+      ALTER TABLE recipes
+      ALTER COLUMN alcohol_strength TYPE integer USING alcohol_strength::integer;
+    SQL
+
+    change_column_default :recipes, :alcohol_strength, 0
+    change_column_null :recipes, :alcohol_strength, false
+  end
+end
