b974a3e8f2de50b5591250225ec1fd3da44ac974
Kaito Ono <kaito@example.com>
2025-01-11 02:05:10 +0900

    材料と分量をセットでDBへ登録できるようにする

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,6 +16,7 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.recipe_ingredients.build
     @base_liquors = BaseLiquor.all
     @difficulties = Difficulty.all
     @ingredients = Ingredient.all
@@ -36,26 +37,26 @@ class RecipesController < ApplicationController
       end
   end
 
-    def my_recipes
-      @recipes = user_signed_in? ? current_user.recipes : []
-    end
+  def my_recipes
+    @recipes = user_signed_in? ? current_user.recipes : []
+  end
 
-    def update
-      @recipe = current_user.recipes.find(params[:id])
+  def update
+    @recipe = current_user.recipes.find(params[:id])
 
-      if @recipe.update(recipe_params)
-        redirect_to @recipe, notice: 'カクテルが更新されました！'
-      else
-        flash.now[:alert] = "カクテルの更新に失敗しました。"
-        render :edit
-      end
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: 'カクテルが更新されました！'
+    else
+      flash.now[:alert] = "カクテルの更新に失敗しました。"
+      render :edit
     end
+  end
 
-    def destroy
-      @recipe = current_user.recipes.find(params[:id])
-      @recipe.destroy
-      redirect_to my_recipes_recipes_path, notice: "カクテルが削除されました"
-    end
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to my_recipes_recipes_path, notice: "カクテルが削除されました"
+  end
 
   private
 
@@ -64,7 +65,8 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, :alcohol_strength, :flavor_id, :procedure, :difficulty_id, :base_liquor_id, ingredient_ids: [])
+    params.require(:recipe).permit(:name, :alcohol_strength, :flavor_id, :procedure, :difficulty_id, :base_liquor_id,
+    recipe_ingredients_attributes: [:ingredient_id, :quantity, :_destroy])
   end
 
   def correct_user
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,10 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  validates :quantity, presence: true, if: :quantity_present?
+
+  def quantity_present?
+    quantity.present?
+  end
 end
