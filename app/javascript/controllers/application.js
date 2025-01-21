d36a3771ccd87d836b7f0877e6171ee90461fc26
Kaito Ono <kaito@example.com>
2025-01-21 12:35:35 +0900

    オートコンプリート途中

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,7 @@
 import { Application } from "@hotwired/stimulus"
-
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 const application = Application.start()
-
+eagerLoadControllersFrom("controllers", application);
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.register("recipes-autocomplete", RecipesAutocompleteController);
 
-export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,9 @@
-pin "application", preload: true  # 最初にアプリケーションをピン
+pin "application", preload: true
 pin "rails-ujs", to: "rails-ujs.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "activestorage", to: "activestorage.js"
 pin "@hotwired/stimulus", to: "@hotwired/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-autocomplete", to: "stimulus-autocomplete.js"
+pin "application", to: "application.js"
