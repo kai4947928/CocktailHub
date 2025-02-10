1ba3c7fabd8efb5eaa0f53c5a4cc26f1a119fab7
Kaito Ono <kaito@example.com>
2025-02-10 12:27:01 +0900

    修正２

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@
 OmniAuth.config.full_host = Rails.env.production? ? "https://cocktail-hub-74469733b659.herokuapp.com" : "http://localhost:3000"
 
 OmniAuth.config.on_failure = proc do |env|
-  SessionsController.action(:failure).call(env)
+  Rack::Response.new(["302 Moved"], 302, "Location" => Rails.application.routes.url_helpers.root_path).finish
 end
 
 OmniAuth.config.allowed_request_methods = [:post]
