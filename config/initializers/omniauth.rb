f6a6d6e57b6bd6237c6b4686791c69d66ed93d88
Kaito Ono <kaito@example.com>
2025-02-28 14:19:29 +0900

    google認証のURI修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-OmniAuth.config.full_host = Rails.env.production? ? "https://cocktail-hub-74469733b659.herokuapp.com" : "http://localhost:3000"
+OmniAuth.config.full_host = Rails.env.production? ? "https://www.cocktail-hub.com" : "http://localhost:3000"
 
 OmniAuth.config.on_failure = proc do |env|
   SessionsController.action(:failure).call(env)
